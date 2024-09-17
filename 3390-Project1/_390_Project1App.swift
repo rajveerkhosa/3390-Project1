b92738fc7e61c6fdfb4dcd19ef31872b314a384e
rajveerkhosa <rajveerk23@icloud.com>
2024-09-16 21:45:02 -0700

    Initial Commit

---
+++ 3390-Project1/_390_Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  _390_Project1App.swift
+//  3390-Project1
+//
+//  Created by Rajveer Singh Khosa on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct _390_Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
